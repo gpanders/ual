9ab50b1ca7cee8b301979e6b1a3d7aafab19cc9b
Greg Anders <greg@gpanders.com>
2019-10-27 18:24:01 -0600

    Remove Markdown support and convert ual to a bash script
    KISS.

--- completions/fish/ual.fish
+++ completions/fish/ual.fish
@@ -1,11 +1,11 @@
-function __fish_complete_notes
-    set -q ual_path; or set -l ual_path "$HOME/.notes"
-    printf "%s\n" (command ls $ual_path | sed 's/\.[^.]*$//')
+function __ual_complete_notes
+    set -q UAL_PATH; or set -l UAL_PATH "$HOME/.notes"
+    printf "%s\n" (command ls $UAL_PATH | sed 's/\.[^.]*$//')
 end
 
-complete -k -x -c ual -n '__fish_seen_subcommand_from rm edit' -a '(__fish_complete_notes)'
+complete -k -x -c ual -n '__fish_seen_subcommand_from rm edit' -a '(__ual_complete_notes)'
 complete -k -x -c ual -n '__fish_seen_subcommand_from sync ls'
-complete -k -x -c ual -n '__fish_use_subcommand' -a '(__fish_complete_notes)'
+complete -k -x -c ual -n '__fish_use_subcommand' -a '(__ual_complete_notes)'
 complete -k -x -c ual -n '__fish_use_subcommand' -a 'rm' -d 'Remove a note'
 complete -k -x -c ual -n '__fish_use_subcommand' -a 'ls' -d 'List all notes'
 complete -k -x -c ual -n '__fish_use_subcommand' -a 'edit' -d 'Edit a note'
